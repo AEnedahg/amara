e475676619cabe2552efdaf5472a9435d07f7997
AEnedahg <enedahanthony54@gmail.com>
2026-09-14 19:24:03 +0100

    Update EmailField.tsx

--- frontend/src/components/auth/signup/EmailField.tsx
+++ frontend/src/components/auth/signup/EmailField.tsx
@@ -32,9 +32,9 @@ export default function EmailField({
                     className={clsx(
                         "border border-[#CED4DA] w-full rounded-lg placeholder:text-[#666666] p-4",
                         errors.email
-                            ? "outline-red-500 bg-red-50 text-red-500 focus:outline-red-500"
+                            ? "outline-red-500 autofill:bg-green-50 bg-red-50 text-red-500 focus:outline-red-500"
                             : email
-                              ? "bg-green-50 outline-green-500 text-green-500 focus:outline-green-500 border-2 border-green-500"
+                              ? "bg-green-50 autofill:bg-green-50 outline-green-500 text-green-500 focus:outline-green-500 border-2 border-green-500"
                               : "",
                     )}
                     placeholder="name@example.com"
