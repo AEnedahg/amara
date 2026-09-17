dd540c048bf81cd5e096063e4f0841cfa488a72b
AEnedahg <enedahanthony54@gmail.com>
2026-09-17 21:40:18 +0100

    Update PasswordField.tsx

--- frontend/src/components/auth/signup/PasswordField.tsx
+++ frontend/src/components/auth/signup/PasswordField.tsx
@@ -42,7 +42,7 @@ export default function PasswordField({
                     className={clsx(
                         "border border-[#CED4DA] focus-within:outline-0 w-full rounded-lg placeholder:text-[#666666] p-4",
                         errors.password
-                            ? "outline-red-500 text-red-500 focus:outline-red-500 bg-red-50"
+                            ? "outline-red-500 text-red-500 focus:outline-red-500 bg-red-50 border-2 border-red-500"
                             : password
                               ? "bg-green-50 outline-green-500 text-green-500 focus:outline-green-500 border-2 border-green-500"
                               : "",
